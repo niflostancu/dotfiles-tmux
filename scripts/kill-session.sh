97b638a8768d44c728eeac5e41138750a87bcb96
Stancu Florin <niflostancu@gmail.com>
2019-08-21 17:32:27 +0300

    moved out capture pane as separate plugin (tmux-workspaces)

--- scripts/kill-session.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Kills the current session and selects the previous one
-
-tmux switch-client -l
-tmux kill-session -t "$(tmux display-message -p "#S")"
-
--- tmux-init.sh
+++ tmux-init.sh
@@ -2,15 +2,19 @@
 # Script for extra tmux initialization that isn't possible with the tmux
 # configuration format alone
 
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+set -e
+exec 2>&1
+TMUX_CONF_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # store the path to the tmux.conf dir (used by other scripts / tmux commands)
-tmux set -g "@tmux-path" "$CURRENT_DIR"
+tmux set -g "@tmux-conf-dir" "$TMUX_CONF_DIR"
 
+# local customizations
+XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-~/.config}
+[[ ! -f "$TMUX_CONF_DIR/local.tmux" ]] || source "$TMUX_CONF_DIR/local.tmux"
+[[ ! -f "$XDG_CONFIG_HOME/.tmux/local.tmux" ]] || source "$XDG_CONFIG_HOME/.tmux/local.tmux"
 
-# load other shell-based config scripts
-
-# TPM (tmux plugin manager):
+# Load TPM (tmux plugin manager):
 TPM_DIR="${XDG_CACHE_HOME:-$HOME/.cache}/tmux/plugins/tpm"
 if [[ ! -d "$TPM_DIR" ]]; then
   echo "Installing Tmux Plugin Manager..."
@@ -19,5 +23,5 @@ fi
 ( source "$TPM_DIR/tpm"; )
 
 # Capture-pane plugin
-source "$CURRENT_DIR/capture-pane/capture.tmux.sh"
+#source "$CURRENT_DIR/capture-pane/capture.tmux.sh"
 
